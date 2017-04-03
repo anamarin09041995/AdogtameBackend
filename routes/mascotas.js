61d8fde43081718b0b6bac30e8e82c2efef4897a
Ana Marin <anamarin@unicauca.edu.co>
2017-04-03 17:48:59 -0500

    Se insertaron metodos post, put y delete

--- routes/mascotas.js
+++ routes/mascotas.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-var ObjectID = require("mongodb").ObjectID;
 
 router.use((req, res, next) => {
     req.collection = req.db.collection("mascotas");
@@ -13,6 +12,38 @@ router.get("/", (req, res, next) => {
     }).catch(err => {
         res.send([]);
     });
+});
+
+router.post("/", (req, res, next) => {
+    let book = req.body;
+    req.collection.insert(book).then(result => {
+        res.send({ success: true });
+    }).catch(err => {
+        res.send({ success: false });
+    });
+});
+
+router.put("/:id", (req, res, next) => {
+    let id = new ObjectID(req.params.id);
+    let book = req.body;
+    req.collection.updateOne({_id:id}, {$set:book}).then(result=>{
+        res.send({success:true});
+    }).catch(err=>{
+        res.send({success:false})
+    });
+
+   
+});
+
+router.delete("/:id", (req, res, next) => {
+    let id = new ObjectID(req.params.id);
+    req.collection.deleteOne({_id:id}).then(result=>{
+        res.send({success:true});
+    })
+    .catch(err=>{
+        res.send({success:false});
+    
+    }); 
 });
 
 module.exports = router;
