2ab55d1234c87c24405b63cb0b2131c26be2c3b9
Ana Marin <anamarin@unicauca.edu.co>
2017-04-05 20:28:44 -0500

    Error en get de seguimmiento

--- routes/seguimiento.js
+++ routes/seguimiento.js
@@ -18,7 +18,7 @@ router.get("/:id", (req, res, next) => {
 router.get("/:id/mascota/:nombre", (req, res, next) => {
     let id =  req.params.id;
     let nombre = req.params.nombre;
-    req.collection.findOne({ "usuario.id":id, nombre: nombre   }).toArray().then(data => {
+    req.collection.findOne({ "usuario.id":id, nombre: nombre   }).then(data => {
         if(data){
             res.send(data);
         }else{
