ffe38d95d55fd51eaecf34d9505a2927555c5871
Ana Marin <anamarin@unicauca.edu.co>
2017-04-10 22:08:07 -0500

    Prueba casi final

--- routes/email.js
+++ routes/email.js
@@ -9,29 +9,30 @@ router.use((req, res, next) => {
 
 router.post("/", (req, res, next) => {
     let email = req.body;
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'anamarin@unicauca.edu.co',
-            pass: '191012seekquiet'
-        }
-    });
-    let mailOptions = {
-        from: '"Fred Foo 👻" <anamarin@unicauca.edu.co>', // sender address
-        to: 'juanmarinmartinez@gmail.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world ?', // plain text body
-        html: '<b>Hello world ?</b>' // html body
-    };
+    res.send(req.body);
+    // let transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: 'anamarin@unicauca.edu.co',
+    //         pass: '191012seekquiet'
+    //     }
+    // });
+    // let mailOptions = {
+    //     from: '"Fred Foo 👻" <anamarin@unicauca.edu.co>', // sender address
+    //     to: 'juanmarinmartinez@gmail.com', // list of receivers
+    //     subject: 'Hello ✔', // Subject line
+    //     text: 'Hello world ?', // plain text body
+    //     html: '<b>Hello world ?</b>' // html body
+    // };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            res.send({ success: false, error: error, info: info });
-            return console.log(error);
-        }
-        res.send({ success: true });
-        console.log('Message %s sent: %s', info.messageId, info.response);
-    });
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //         res.send({ success: false, error: error, info: info });
+    //         return console.log(error);
+    //     }
+    //     res.send({ success: true });
+    //     console.log('Message %s sent: %s', info.messageId, info.response);
+    // });
 
     // req.collection.insert(email).then(result => {
     //     res.send({ success: true });
