3191793525aec5bd36f00d4799fd312873ffad01
Ana Marin <anamarin@unicauca.edu.co>
2017-04-10 21:36:05 -0500

    Prueba07

--- routes/email.js
+++ routes/email.js
@@ -10,12 +10,12 @@ router.use((req, res, next) => {
 router.post("/", (req, res, next) => {
     let email = req.body;
     let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'gmail.user@gmail.com',
-        pass: 'yourpass'
-    }
-});
+        service: 'gmail',
+        auth: {
+            user: 'anamarin@unicauca.edu.co',
+            pass: '191012seekquiet'
+        }
+    });
     req.collection.insert(email).then(result => {
         res.send({ success: true });
     }).catch(err => {
