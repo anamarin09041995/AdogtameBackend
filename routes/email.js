2b295165ff0891e1ba7841aa74cb66dea7718760
Ana Marin <anamarin@unicauca.edu.co>
2017-04-10 21:32:18 -0500

    Prueba 30

--- routes/email.js
+++ routes/email.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 router.use((req, res, next) => {
     req.collection = req.db.collection("email");
@@ -8,6 +9,13 @@ router.use((req, res, next) => {
 
 router.post("/", (req, res, next) => {
     let email = req.body;
+    let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'gmail.user@gmail.com',
+        pass: 'yourpass'
+    }
+});
     req.collection.insert(email).then(result => {
         res.send({ success: true });
     }).catch(err => {
