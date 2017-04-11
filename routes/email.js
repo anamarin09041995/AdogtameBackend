588609735455f6498e53197fb06cd4928af3228a
Ana Marin <anamarin@unicauca.edu.co>
2017-04-10 21:02:23 -0500

    Prueba insercion en post dde mail

--- routes/email.js
+++ routes/email.js
@@ -9,28 +9,29 @@ router.use((req, res, next) => {
 
 router.post("/email", (req, res, next) => {
     let email = req.body;
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'anamarin@unicauca.edu.co',
-            pass: '191012seekquiet'
-        }
-    });
+    res.send(req.body);
+    // let transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: 'anamarin@unicauca.edu.co',
+    //         pass: '191012seekquiet'
+    //     }
+    // });
     req.collection.insert(email).then(result => {
         res.send({ success: true });
-        let mailOptions = {
-            from: 'anamarin@unicauca.edu.co', // sender address
-            to: 'anamarin@unicauca.edu.co', // list of receivers
-            subject: email.subject, // Subject line
-            text: email.text, // plain text body
-            html: email.html // html body
-        };
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message %s sent: %s', info.messageId, info.response);
-        });
+        // let mailOptions = {
+        //     from: '<anamarin@unicauca.edu.co>', // sender address
+        //     to: '<anamarin@unicauca.edu.co>', // list of receivers
+        //     subject: email.subject, // Subject line
+        //     text: email.text, // plain text body
+        //     html: email.html // html body
+        // };
+        // transporter.sendMail(mailOptions, (error, info) => {
+        //     if (error) {
+        //         return console.log(error);
+        //     }
+        //     console.log('Message %s sent: %s', info.messageId, info.response);
+        // });
     }).catch(err => {
         res.send({ success: false });
     });
