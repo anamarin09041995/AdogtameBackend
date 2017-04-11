9bf97a2b2fb4cc01465d262cb6633a4509725363
Ana Marin <anamarin@unicauca.edu.co>
2017-04-11 12:50:01 -0500

    Prueba con email 5764

--- routes/email.js
+++ routes/email.js
@@ -9,36 +9,34 @@ router.use((req, res, next) => {
 
 router.post("/", (req, res, next) => {
     let email = req.body;
+    let transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+              user: 'anamarin@unicauca.edu.co',
+              pass: '191012seekquiet'
+          }
+      });
+     let mailOptions = {
+         from: ' <anamarin@unicauca.edu.co>', 
+         to: 'lhanier@unicauca.edu.co', 
+         subject: 'Hello ✔', 
+         text: 'Hello world ?', 
+         html: '<b>Hello world ?</b>'
+     };
+
+     transporter.sendMail(mailOptions, (error, info) => {
+         if (error) {
+             res.send({ success: false, error: error, info: info });
+             return console.log(error);
+         }
+         res.send({ success: true });
+         console.log('Message %s sent: %s', info.messageId, info.response);
+     });
      req.collection.insert(req.body).then(result => {
          res.send({ success: true });
      }).catch(err => {
          res.send({ success: false });
-     });
-    // let transporter = nodemailer.createTransport({
-    //      service: 'gmail',
-    //      auth: {
-    //          user: 'anamarin@unicauca.edu.co',
-    //          pass: '191012seekquiet'
-    //      }
-    //  });
-    // let mailOptions = {
-    //     from: ' <anamarin@unicauca.edu.co>', 
-    //     to: 'juanmarinmartinez@gmail.com', 
-    //     subject: 'Hello ✔', 
-    //     text: 'Hello world ?', 
-    //     html: '<b>Hello world ?</b>'
-    // };
-
-    // transporter.sendMail(mailOptions, (error, info) => {
-    //     if (error) {
-    //         res.send({ success: false, error: error, info: info });
-    //         return console.log(error);
-    //     }
-    //     res.send({ success: true });
-    //     console.log('Message %s sent: %s', info.messageId, info.response);
-    // });
-
-   
+     });   
 });
 
 // send mail with defined transport object
